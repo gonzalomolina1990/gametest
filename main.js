4ac266e64580f9f6e302b5e2958fee31ea18cf8a
gonzalo.molina@wundermanthompson.com <gonzalo.molina@wundermanthompson.com>
2023-02-13 15:15:58 -0300

    more gameable

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let isJumping = false;
 let score = 0;
 let isFalling = true;
+let isPlaying = true;
 
 function start() {
   setTimeout(() => {
-    gsap.fromTo("#player", {y: -500, ease: Power2.easeIn}, {y: 0, duration:1, ease: Power2.easeIn});
+    gsap.fromTo("#player", {y: -400, ease: Power2.easeIn}, {y: 0, duration:6, ease: Power2.easeIn});
   }, 100);
+
+  setInterval(() => {
+    if (document.getElementById('player').style.transform === "translate(0px, 0px)") {
+      isPlaying = false;
+      checkPlayerPosition();
+    }
+  }, 10);
 }
 
 function jump() {
@@ -31,12 +39,22 @@ function updateScore() {
 
 function checkPlayerPosition() {
   const player = document.getElementById('player');
-  const playerBottom = player.getBoundingClientRect().bottom;
   const game = document.getElementById('game');
-  const gameBottom = game.getBoundingClientRect().bottom;
+  const playerPosition = player.getBoundingClientRect();
+  const gamePosition = game.getBoundingClientRect();
 
-  if (playerBottom >= gameBottom) {
-    alert("You lost! Your score was " + score);
+  if (document.getElementById('player').style.transform === "translate(0px, 0px)") {
+    isPlaying = false;
+    gsap.to("#player", {scale: 0, duration: 0.2, ease: Power2.easeIn, onComplete: function name(params) {
+      player.style.display = "none";
+    }});
+   
+    document.querySelector("#text").innerHTML = "You lost! Your score was " + score;
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 3000);
+
   } else if (isFalling) {
     gsap.to("#player", {y: "+=10px", duration: 0.1, ease: Power2.easeIn});
     setTimeout(checkPlayerPosition, 100);
@@ -45,8 +63,10 @@ function checkPlayerPosition() {
 
 setTimeout(() => {
     const player = document.getElementById('player');
-    player.addEventListener('click', jump);
-    player.addEventListener('touchstart', jump);
+    if (isPlaying) {
+      player.addEventListener('click', jump);
+      player.addEventListener('touchstart', jump);
+    } 
 
 }, 2000);
 
