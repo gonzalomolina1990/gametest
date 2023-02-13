0638783445970011916e0dc0734dc9c07eb587ab
gonzalo.molina@wundermanthompson.com <gonzalo.molina@wundermanthompson.com>
2023-02-13 10:38:24 -0300

    small improvements

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
 let isJumping = false;
+let score = 0;
+let isFalling = true;
+
+function start() {
+  setTimeout(() => {
+    gsap.fromTo("#player", {y: -500, ease: Power2.easeIn}, {y: 0, duration:1, ease: Power2.easeIn});
+  }, 100);
+}
 
 function jump() {
   if (!isJumping) {
     isJumping = true;
+    isFalling = false;
 
     gsap.to("#player", {y: "-=200px", duration:2, ease: Power2.easeOut});
 
     setTimeout(() => {
         gsap.to("#player", {y: 0, duration: 1, ease: Power2.easeIn});
         isJumping = false;
-
+        isFalling = true;
+        score += 1;
+        updateScore();
     }, 2000);
-  }
+  } 
 }
 
+function updateScore() {
+  document.querySelector('.score').innerHTML = "Score: " + score;
+}
 
+function checkPlayerPosition() {
+  const player = document.getElementById('player');
+  const playerBottom = player.getBoundingClientRect().bottom;
+  const game = document.getElementById('game');
+  const gameBottom = game.getBoundingClientRect().bottom;
 
-
+  if (playerBottom >= gameBottom) {
+    alert("You lost! Your score was " + score);
+  } else if (isFalling) {
+    gsap.to("#player", {y: "+=10px", duration: 0.1, ease: Power2.easeIn});
+    setTimeout(checkPlayerPosition, 100);
+  }
+}
 
 setTimeout(() => {
     const player = document.getElementById('player');
@@ -25,5 +50,4 @@ setTimeout(() => {
 
 }, 2000);
 
-console.log(document.querySelector('.player'));
-
+start();
